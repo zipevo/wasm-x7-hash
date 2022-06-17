ca51949fe05cf383ed6abdd6a9ce46a5745923bc
markin.io <markin.io210@gmail.com>
2022-06-17 17:56:31 +0400

    feat: initial

---
+++ x11_bindings.c
@@ -0,0 +1,23 @@
+#include "emscripten.h"
+#include "lib/x11.h"
+
+EMSCRIPTEN_KEEPALIVE
+// TODO: replace with uint32_t ?
+// https://stackoverflow.com/questions/20077313/uint32-t-vs-int-as-a-convention-for-everyday-programming
+uint8_t* create_buffer(int size) {
+  return malloc(size * sizeof(char));
+}
+
+EMSCRIPTEN_KEEPALIVE
+void destroy_buffer(uint8_t* p) {
+  free(p);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void digest(uint8_t* p_in, uint8_t* p_out, uint32_t input_size) {
+    // https://stackoverflow.com/questions/42961443/convert-from-uint8-t-to-char-in-c
+    char * input = (char *) p_in;
+    char * output = (char *) p_out;
+
+    x11_hash(input, output, input_size);
+}
